b64697e6b0c7e8128466118e295721b4609f3e05
cloudsoldier <36954152+cloudsoldier@users.noreply.github.com>
2022-10-18 12:00:06 +0100

    Update azureprovider.tf

--- azureprovider.tf
+++ azureprovider.tf
@@ -11,6 +11,6 @@ version = ">= 2.0" # Optional but recommended in production
 provider "azurerm" {
 features {}
 
-subscription_id = "4f9fc577-70d6-4b0c-b0c9-3c7f24dae85f"
-tenant_id       =  "b8b4c61c-f1ca-4aff-a0bd-9c6f01c3eca5"
+subscription_id = "<xxxxxx-xxxxx-xxxxx-xxxx>"
+tenant_id       =  "<xxxxxx-xxxxx-xxxxx-xxxx>"
 }
