408a77f1a228d6942996bf71101c3251af85bb16
cloudsoldier <36954152+cloudsoldier@users.noreply.github.com>
2022-10-18 12:56:22 +0100

    Update variables.tf

--- ADM_VM_withexisting/variables.tf
+++ ADM_VM_withexisting/variables.tf
@@ -1,1 +1,17 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Resource_group_name"
+}
+variable "virtual_network_name" {
+  type        = string
+  description = "virtual_network_name"
+}
+variable "server_subnet_name" {
+  type        = string
+  description = "server_subnet_name"
+}
+
+variable "location" {
+  type = string
+}
 
